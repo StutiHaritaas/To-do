acbb2433df56e36e94e2411c0811923e29fcd92f
Stuti Haritaas <stuti_201900330@smit.smu.edu.in>
2021-11-13 22:43:42 +0530

    my next commit

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -5,13 +5,13 @@ import { Injectable } from '@angular/core';
 })
 export class TodoService {
   details = [
-    { Task: 'Go to the library', Preference: 2 },
-
-    { Task: 'Read a novel', Preference: 4 },
-
     { Task: 'Attend the classes', Preference: 1 },
 
+    { Task: 'Go to the library', Preference: 2 },
+
     { Task: 'Complete the asssigned project', Preference: 3 },
+
+    { Task: 'Read a novel', Preference: 4 },
   ];
 
   getDetails() {
